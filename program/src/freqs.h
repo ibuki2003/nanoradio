ac2ef276f5db9de61d360ce2225b65752a49a761
ibuki2003 <main@fuwa.dev>
2024-08-22 22:07:53 +0900

    feat: control UI

--- program/src/freqs.h
+++ program/src/freqs.h
@@ -7,8 +7,11 @@ typedef struct FreqMemory {
   const char* name;
 } FreqMemory;
 
-extern const FreqMemory* const FREQS_AM[8];
-extern const FreqMemory* const FREQS_FM[8];
+#define AREA_COUNT 8
+
+extern const FreqMemory* const FREQS_AM[AREA_COUNT];
+extern const FreqMemory* const FREQS_FM[AREA_COUNT];
+extern const char* const AREAS[AREA_COUNT];
 
 void find_station_refresh(uint16_t freq, bool am, uint8_t area);
 char const* find_station(uint16_t freq, bool am, uint8_t area);
--- program/src/main.c
+++ program/src/main.c
@@ -8,9 +8,9 @@
 #include <i2c.h>
 #include <lcd.h>
 #include <kt0913.h>
-
-#include "freqs.h"
-#include "utils.h"
+#include <freqs.h>
+#include <utils.h>
+#include <lcd_kana.h>
 
 // will be called on startup and after wake-up
 void setup() {
@@ -42,14 +42,21 @@ void setup() {
   i2c_setup();
 }
 
+typedef enum {
+  MODE_NORMAL = 0,
+  MODE_FREQ,
+  MODE_CONFIG,
+  MODE_SLEEP,
+} Mode;
+
 // global state
-bool am = true;
+bool am = false;
 uint16_t am_freq = 594;
 uint16_t fm_freq = 847;
 uint8_t vol = 16;
 uint8_t area = 2; // kanto
 
-void draw_l1() {
+void draw_l1(bool seeking) {
   uint8_t sbuf[9];
   sbuf[0] = 0x40;
 
@@ -77,13 +84,17 @@ void draw_l1() {
     sbuf[8] = 'M';
   }
 
+  if (seeking) {
+    if (sbuf[3] == ' ') sbuf[3] = 0xfb;
+    sbuf[8] = 0xfc;
+  }
+
   LCD_send_command(0x80);
   i2c_send(I2C_LCD_ADDR, sbuf, 9);
 }
 
 void draw_station_name() {
   uint8_t sbuf[9];
-  for (uint i = 1; i < 9; ++i) sbuf[i] = ' ';
   sbuf[0] = 0x40;
 
   char const* const name = find_station(am ? am_freq : fm_freq, am, area);
@@ -91,6 +102,7 @@ void draw_station_name() {
     my_strcpy(&sbuf[1], (const uint8_t*)name);
   } else {
     sbuf[1] = '-';
+    for (uint i = 2; i < 9; ++i) sbuf[i] = ' ';
   }
 
   // on line 2
@@ -114,12 +126,182 @@ void add_freq(int8_t d) {
 
 #define LOOP_INTERVAL 10
 
+#define VOLUME_SHOW_TIMEOUT 50
+Mode run_normal() {
+  // initialize
+  draw_l1(false);
+  draw_station_name();
+
+  uint16_t volume_timeout = 0;
+
+  while(1) {
+
+    uint8_t k = get_buttons();
+    bool timedout = volume_timeout == 1;
+    if (volume_timeout) volume_timeout--;
+
+    if (k || timedout) {
+
+      if (button_hold[0] % 8 == 1) {
+        if (vol > 0) --vol;
+        KT0913_set_vol(vol);
+        volume_timeout = VOLUME_SHOW_TIMEOUT;
+      }
+      if (button_hold[2] % 8 == 1) {
+        if (vol < 31) ++vol;
+        KT0913_set_vol(vol);
+        volume_timeout = VOLUME_SHOW_TIMEOUT;
+      }
+
+      if (button_release[1]) {
+        return MODE_FREQ;
+      }
+
+      if (button_hold[1] >= 200) {
+        mark_buttons_handled(0b010);
+        // go to sleep
+        // turn off FET
+        funDigitalWrite(PC4, 1);
+        // wait for release
+        while (funDigitalRead(PA1) == 0) {}
+        return MODE_SLEEP;
+      }
+
+      // draw
+
+      // freq won't change
+      // draw_l1(false);
+
+      if (volume_timeout) {
+        uint8_t sbuf[9];
+        sbuf[0] = 0x40;
+        for (uint i = 1; i < 9; ++i) sbuf[i] = ' ';
+
+        uint16_t v = vol;
+        if (v == 1) v = 0;
+        uint8_t bar_len = v / 5;
+        if (bar_len)
+          for (uint i = 0; i < bar_len; ++i)
+            sbuf[i+1] = CHAR_BLACK;
+        sbuf[bar_len+1] = CHAR_BAR_PART;
+
+        LCD_make_bar_font(v % 5);
+
+        v = vol == 31 ? 99 : vol * 100 / 31;
+        my_itoa(v, &sbuf[8], 2);
+
+        LCD_send_command(0x80 | 0x40);
+        i2c_send(I2C_LCD_ADDR, sbuf, 9);
+      } else {
+        draw_station_name();
+      }
+    }
+
+    Delay_Ms(LOOP_INTERVAL);
+  }
+}
+
+#define FREQ_TIMEOUT 200
+Mode run_freq() {
+  uint16_t timeout = FREQ_TIMEOUT;
+  // initialize
+  draw_l1(true);
+  draw_station_name();
+
+  while(1) {
+
+    uint8_t k = get_buttons();
+
+    if (k) {
+      timeout = FREQ_TIMEOUT;
+      if (button_hold[0] == 1 || button_hold[0] >= 50) {
+        add_freq(-1);
+      }
+      if (button_hold[2] == 1 || button_hold[2] >= 50) {
+        add_freq(1);
+      }
+
+      if (button_release[1]) {
+        am = !am;
+        KT0913_set_amfm(am);
+      }
+
+      if (button_hold[1] >= 200) {
+        mark_buttons_handled(0b010);
+        // go to config
+        return MODE_CONFIG;
+      }
+
+      // draw
+      draw_l1(true);
+      draw_station_name();
+    }
+
+    if (--timeout == 0) {
+      return MODE_NORMAL;
+    }
+
+    Delay_Ms(LOOP_INTERVAL);
+  }
+}
+
+#define CONFIG_TIMEOUT 1000
+Mode run_config() {
+  uint16_t timeout = CONFIG_TIMEOUT;
+  // initialize
+
+  uint8_t sbuf[9];
+  sbuf[0] = 0x40;
+
+  LCD_send_command(0x80);
+  i2c_send(I2C_LCD_ADDR, (const uint8_t*)("\x40" ｴ ﾘ ｱ ｾ ｯ ﾃ ｲ " "), 9);
+
+
+  my_strcpy(&sbuf[1], (const uint8_t*)AREAS[area]);
+  LCD_send_command(0x80 | 0x40);
+  i2c_send(I2C_LCD_ADDR, sbuf, 9);
+
+  while(1) {
+
+    uint8_t k = get_buttons();
+
+    if (k) {
+      timeout = FREQ_TIMEOUT;
+      if (button_hold[0] % 16 == 1) {
+        area = (area + AREA_COUNT - 1) % AREA_COUNT;
+      }
+      if (button_hold[2] == 1 || button_hold[2] >= 50) {
+        area = (area + 1) % AREA_COUNT;
+      }
+
+      if (button_release[1] || button_hold[1] >= 200) {
+        mark_buttons_handled(0b010);
+        return MODE_NORMAL;
+      }
+
+      // draw
+      my_strcpy(&sbuf[1], (const uint8_t*)AREAS[area]);
+      LCD_send_command(0x80 | 0x40);
+      i2c_send(I2C_LCD_ADDR, sbuf, 9);
+    }
+
+    if (--timeout == 0) {
+      return MODE_NORMAL;
+    }
+
+    Delay_Ms(LOOP_INTERVAL);
+  }
+}
+
 void inner_main() {
   // here cpu is after power-up or wake-up
   setup();
 
   Delay_Ms(300);
-  get_buttons(); // reset button state
+
+  // reset button state
+  get_buttons();
+  mark_buttons_handled(0b111);
 
   // Initialize IC
   KT0913_init();
@@ -128,50 +310,29 @@ void inner_main() {
   // Initialize LCD
   LCD_init();
 
-  uint8_t buf[2];
-  uint8_t cnt = 0;
-  uint8_t sbuf[9];
-
   KT0913_set_amfm(am);
   KT0913_set_freq_fm(fm_freq);
 
-  draw_l1();
+  find_station_refresh(am ? am_freq : fm_freq, am, area);
 
-  uint8_t v;
-  while(1) {
-
-    uint8_t k = get_buttons();
-
-    if (k) {
-      if (button_hold[0] == 1 || button_hold[0] >= 50) {
-        add_freq(-1);
-        draw_l1();
-        draw_station_name();
-      }
-      if (button_release[1]) {
-        am = !am;
-        KT0913_set_amfm(am);
-        add_freq(0);
-        draw_l1();
-        draw_station_name();
-      }
-      if (button_hold[1] >= 100) {
-        // go to sleep
-        // turn off FET
-        funDigitalWrite(PC4, 1);
-        // wait for release
-        while (funDigitalRead(PA1) == 0) {}
+  Mode mode = MODE_NORMAL;
+  while (true) {
+    switch (mode) {
+      case MODE_NORMAL:
+        mode = run_normal();
+        break;
+      case MODE_FREQ:
+        mode = run_freq();
+        break;
+      case MODE_CONFIG:
+        mode = run_config();
+        break;
+      case MODE_SLEEP:
         Delay_Ms(100);
         sleep();
-        return; // setup again
-      }
-      if (button_hold[2] == 1 || button_hold[2] >= 50) {
-        add_freq(1);
-        draw_l1();
-        draw_station_name();
-      }
+        return; // Run setup again
+        break;
     }
-
     Delay_Ms(LOOP_INTERVAL);
   }
 
